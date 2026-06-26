2a030ebd36b23c71ab80967aabe9f52d293db36e
Patrick Dubois <pd5023@gmail.com>
2026-06-26 09:17:58 -0700

    Fix config file encoding: remove UTF-8 BOM that broke Vite build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-﻿export default {
+export default {
   content: ["./index.html","./src/**/*.{js,jsx}"],
   theme: { extend: { colors: { brand: { DEFAULT:"#1976D2", light:"#E3F2FD", teal:"#26A69A" } } } },
   plugins: []
